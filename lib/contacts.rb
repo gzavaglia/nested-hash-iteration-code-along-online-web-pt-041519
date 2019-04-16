fd97c8e9bd97a79d6872cbfce59b81c3e2e6725e
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-16 16:27:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,12 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts["Freddy Mercury"].each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        value.delete_if do |flavor|
-          flavor == "strawberry"
-        end #end do 
-       end #end if 
-  end #end do 
-  contacts
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
